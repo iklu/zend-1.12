7e1240def1e62f42a4ae773d7278d1a332c7ec8b
Dragoi Ovidiu <dragoiovidiu2011@yahoo.com>
2017-05-03 15:46:48 +0300

    added DI support

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -10,18 +10,29 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view = $this->getResource('view');
         $view->doctype('XHTML1_STRICT');
     }
+    
+    protected function _initRoutes()
+    {
+        $router = Zend_Controller_Front::getInstance()->getRouter();
+        
+        $route = new Zend_Controller_Router_Route( 'author', array('controller' => 'user', 'action' => 'indsex'));
+        
+        $router->addRoute('author', $route);
+    }
 
     /**
      * Initialize the dependency injection container
      */
     protected function _initDependencyInjection()
     {
-        $container = \DI\ContainerBuilder::buildDevContainer();
+       $builder = new \DI\ContainerBuilder();
+        $builder->useAnnotations(true);
+        $container = $builder->build();
 
-        $dispatcher = new Dispatcher();
+        $dispatcher = new \DI\Bridge\ZendFramework1\Dispatcher();
         $dispatcher->setContainer($container);
 
-        $frontController = Zend_Controller_Front::getInstance();
-        $frontController->setDispatcher($dispatcher);
+        Zend_Controller_Front::getInstance()->setDispatcher($dispatcher);
+        
     }
 }
